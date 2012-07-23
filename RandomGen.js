ca7357dc818dfa73d2b44cb1d76caaf600eb3141
unknown <ukcmat@mm4162.corp.waters.com>
2012-07-23 09:23:23 +0100

    added flot sub module

---
+++ RandomGen.js
@@ -0,0 +1,13 @@
+var Things = new Array();
+var fs = require('fs');
+
+
+for (var i = 10000 - 1; i >= 0; i--) {
+	Things[i] = new Array();
+	Things[i][0] = i;
+	Things[i][1] = Math.random();
+};
+
+console.log(Things);
+
+fs.writeFileSync('d1.json', 'var d1 = '+JSON.stringify(Things)+';', 'utf8');
