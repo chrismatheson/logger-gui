2b7691dfc1173fc553250fa7e0f21005a4ae5c2f
unknown <ukcmat@mm4162.corp.waters.com>
2012-07-27 12:21:12 +0100

    deleteing files

--- RandomGen.js
+++ RandomGen.js
@@ -2,7 +2,7 @@ var Things = new Array();
 var fs = require('fs');
 
 
-for (var i = 10000 - 1; i >= 0; i--) {
+for (var i = 1000 - 1; i >= 0; i--) {
 	Things[i] = new Array();
 	Things[i][0] = i;
 	Things[i][1] = Math.random();
